fbc218b4f1a19b7749af19e7b36c40ab4a6d5c1e
Chetan Shetty <chetan.shetty3003@gmail.com>
2024-02-06 15:19:42 +0530

    daily check-in #1

---
+++ Prepare/Tutorials/30 Days of Code/C#/Day 1 - Data Types.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        // Declare second integer, double, and String variables.
+        // Read and save an integer, double, and String to your variables.
+        int i2 = Convert.ToInt32(Console.ReadLine());
+        double d2 = Convert.ToDouble(Console.ReadLine());
+        string s2 = Console.ReadLine();
+
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(i + i2);
+
+        // Print the sum of the double variables on a new line.
+        double sum = d + d2;
+        Console.WriteLine(sum.ToString("0.0"));
+
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+        Console.WriteLine(s + s2);
+    }
+}
